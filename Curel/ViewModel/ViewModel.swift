d43c5ec402ea562b19aab2f4c507fe676acb381f
HTBRO98 <srty.mv.fu@gmail.com>
2021-03-07 13:59:34 +0900

    03/06 14:00

--- Curel/ViewModel/ViewModel.swift
+++ Curel/ViewModel/ViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import AlamofireImage
 
 class FetchProvider {
     
@@ -14,7 +15,10 @@ class FetchProvider {
         // TODO:apiのurlを合わせる search: tokyo
         // TODO:OpenWeathermap Current weather dataのWeb API
         //"https://api.openweathermap.org/data/2.5/weather?q=london&appid=cdc976a8dbd650139d902d1369ac8840"
-        let baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=\(search)&appid=\(model.apiKey)"
+        //今の天気
+//        let baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=\(search)&appid=\(model.apiKey)"
+        //天気予報
+        let baseUrl = "https://api.openweathermap.org/data/2.5/forecast?q=\(search)&appid=\(model.apiKey)"
         let parameters: [String: Any] = [:]
         let headers: HTTPHeaders = ["Content-Type": "application/json"]
         AF.request(baseUrl, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: headers, interceptor: nil, requestModifier: nil).response(queue: DispatchQueue.global(qos: .utility)) {
@@ -36,12 +40,20 @@ class FetchProvider {
     func jDecode(data: Data?, model: Model) {
         guard let data = data else { return }
         do {
-            let data = try JSONDecoder().decode(WeatherData.self, from: data)
+            let data = try JSONDecoder().decode(Forecast.self, from: data)
             print("jDecode: \(data)")
             model.dataList.append(data)
         } catch let error {
             print("jDecode error: \(error)")
         }
+    }
+    
+    func fetchIconAPI(iPath:String, imageview: UIImageView, placeHolderImage: UIImage) {
+        let iUrl = "https://openweathermap.org/img/wn/\(iPath)@2x.png"
+        guard let url = URL(string: iUrl) else { return }
+        imageview.af.setImage(withURL: url,
+                                      placeholderImage: placeHolderImage,
+                                      imageTransition: .crossDissolve(0.3))
     }
     
 }
