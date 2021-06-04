ce73b709907685d60a99c1d0685cb228b860986f
HTBRO98 <srty.mv.fu@gmail.com>
2021-06-05 08:57:33 +0900

    initial Location Controller

---
+++ CurelWatch Extension/LocationController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationController.swift
+//  CurelWatch Extension
+//
+//  Created by HAYATOYAMAMOTo on 2021/06/05.
+//
+
+import WatchKit
+import CoreLocation
+
+class LocationController: <#super class#> {
+    <#code#>
+}
